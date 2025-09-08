289516426b2d87a1920a9551f497108e8ee1c16b
Oliver Olsen <23oolsen@gmail.com>
2025-09-07 19:47:25 -0600

    basic astar and some restructuring

--- astar.py
+++ astar.py
@@ -1,31 +1,54 @@
-class AStar():
-    def __init__(self):
-        pass
-    def pop_min(self):
-        if not self.queue:
-            return None
-        
+import heapq
+from helpers import *
+import itertools
 
+class AStar():
+    def __init__(self, heuristic):
+        self.goal_map = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        self.goal_positions = {tile: (r, c) for r, row in enumerate(self.goal_map) for c, tile in enumerate(row) if tile != 0}
+        self.h = heuristic
+
+    def heuristic(self, node):
+        return self.h(node)
+
+    def cost(self, node, action, next_node):
+        return 1
         
-        # pops the queue with the least amount of cost TODO
-        return self.queue.pop()
-    def c(self, a, b, c):
-        pass
-    def g(self, a):
-        pass
-    def h(self, a):
-        pass
     def main(self, s0):
-        self.queue = []
-        best_g = {}
-        best_g[s0] = 0
-        while self.queue:
-            n = self.pop_min()
-            if self.goal_test(n.state):
-                return n#solution path from s0 to n_state TODO
+        frontier = []
+        s0.g = 0
+        f0 = s0.g + self.heuristic(s0)
+        
+        tie_breaker = itertools.count()
+        
+        heapq.heappush(frontier, (f0, next(tie_breaker), s0))
+        
+        best_g = {s0: 0}
+        
+        while frontier:
+            f, _, n = heapq.heappop(frontier)
+            
+            if goal_test(n):
+                return n
+            
             for a in n.actions():
                 s_ = n.transition(a)
-                g_ = self.g(n) + self.c(n.state, a, s_)
+                g_ = n.g + self.cost(n, a, s_)
+                
                 if s_ not in best_g or g_ < best_g[s_]:
                     best_g[s_] = g_
-                    self.push(s_, g_ + self.h(s_))
+                    s_.g = g_
+                    f_ = g_ + self.heuristic(s_)
+                    heapq.heappush(frontier, (f_, next(tie_breaker), s_))
+        
+        return None
+    
+def main():
+    u = AStar(h2)
+    s0 = Node()
+    s0.map = [[1,2,3],[4,5,6],[0,7,8]]
+    n = u.main(s0)
+    print(n.prev_actions)
+
+if __name__ == "__main__":
+    main()
--- ucs.py
+++ ucs.py
@@ -1,62 +1,4 @@
-import copy
-
-class Node():
-    def __init__(self):
-        self.map = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        self.prev_actions = []
-        self.g = 0
-
-    def transition(self, a):
-        new_node = copy.deepcopy(self)
-        a = a.lower()
-        row, col = new_node.find_zero()
-
-        if a == "up":
-            new_node.map[row][col], new_node.map[row - 1][col] = new_node.map[row - 1][col], new_node.map[row][col]
-        elif a == "down":
-            new_node.map[row][col], new_node.map[row + 1][col] = new_node.map[row + 1][col], new_node.map[row][col]
-        elif a == "left":
-            new_node.map[row][col], new_node.map[row][col - 1] = new_node.map[row][col - 1], new_node.map[row][col]
-        elif a == "right":
-            new_node.map[row][col], new_node.map[row][col + 1] = new_node.map[row][col + 1], new_node.map[row][col]
-        
-        new_node.prev_actions.append(a)
-        return new_node
-
-    def find_zero(self):
-        x = 0
-        for a in self.map:
-            y = 0
-            for b in a:
-                if b == 0:
-                    return (x, y)
-                y += 1
-            x += 1
-        raise Exception("Zero not found") 
-
-    def actions(self):
-        actions = ["up", "down", "left", "right"]
-        row, col = self.find_zero()
-
-        if row == 0:
-            actions.remove("up")
-        elif row == 2:
-            actions.remove("down")
-        if col == 0:
-            actions.remove("left")
-        elif col == 2:
-            actions.remove("right")
-        
-        return actions
-
-    def __str__(self):
-        return str(self.map)
-
-    def __eq__(self, other):
-        return self.map == other.map
-
-    def __hash__(self):
-        return hash(tuple(tuple(row) for row in self.map))
+from helpers import Node, goal_test
 
 class UCS():
     def __init__(self):
@@ -94,10 +36,6 @@ class UCS():
 
     def cost(self, state, action):
         return 1
-                
-GOAL = Node()
-def goal_test(n):
-    return n == GOAL
 
 def main():
     u = UCS()
